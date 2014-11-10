9d757030b479054edad2e011d0c3d65e7e5082be
panther <tripod320@gmail.com>
2014-11-09 19:36:06 -0600

    bugs fixed!

--- TeXRMPackage/TeXRMPackage.sh
+++ TeXRMPackage/TeXRMPackage.sh
@@ -4,5 +4,15 @@ sleep 2s
 echo "What is this package called?"
 read PACK
 cd /usr/share/texlive/texmf-dist/tex/latex
-sudo rm -rf $PACK
+if [ -f $PACK ] 
+then rm -rf $PACK
+else echo "Not in /usr/share/texlive/texmf-dist/tex/latex"
+fi
+
+cd /usr/share/texmf/tex/latex
+if [ -f $PACK ] then
+then	rm -rf $PACK
+else echo "Non in /usr/share/texmf/tex/latex"
+fi
+
 sudo texhash
