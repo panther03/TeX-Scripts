9d1ec02c3819f937dc8bd1ebd05cd1fff4abdc6e
Julien de Castelnau <tripod320@gmail.com>
2015-03-02 12:40:24 -0600

    Fixed bug

--- TeXAddPackage/TeXAddPackageFTP.sh
+++ TeXAddPackage/TeXAddPackageFTP.sh
@@ -3,7 +3,11 @@ echo "Welcome to the LaTeX package fetcher."
 sleep 2s
 echo "What is this .zip package called?"
 read PACK
-cd /usr/share/texlive/texmf-dist/tex/latex 
+if [ -d /usr/share/texlive/texmf-dist/tex/latex];
+then cd /usr/share/texlive/texmf-dist/tex/latex 
+else cd /usr/share/texmf-dist/tex/latex
+fi
+
 if [ -d $PACK ];
 	then echo "Package already resides on the system." && exit
 	else sudo wget ftp://mirrors.ucr.ac.cr/CTAN/macros/latex/contrib/$PACK.zip
